32f99e04e37b758aec5d2eb16ea7ab5ce7006346
Daniel Batista <dani_batista20@hotmail.com>
2024-05-29 01:47:58 +0100

    upd: anime page - render data

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
-import { data } from '@/app/lib/placeholder-data'
+// import { data } from '@/app/lib/placeholder-data'
+import { fetchAnime } from '@/app/lib/actions'
+
 import { Hero } from '@/components/hero'
-import { AnimeCard } from '@/components/anime'
+import { AnimeCard, AnimeCardProps } from '@/components/anime'
 import { LodingSpinner } from '@/components/loading-spinner'
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetchAnime()
+
   return (
     <main className="flex h-full flex-col items-center max-w-6xl mx-auto px-2 gap-20">
       <Hero />
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((el, i) => (
-          <AnimeCard key={el.id} anime={el} index={i} />
+        {data.map((item: AnimeCardProps, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
         ))}
       </section>
       <LodingSpinner />
